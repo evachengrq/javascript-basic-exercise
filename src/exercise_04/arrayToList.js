15b8c5b380e9db533f67ef5734bbc7449e8f48e2
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-08 17:26:53 +0800

    [evacheng]feat: completed array_to_list_spec

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,15 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  if (array.length === 1) {
+    return { value: array[0], next: null };
+  }
+  return { value: array[0], next: arrayToList(array.slice(1)) };
 }
